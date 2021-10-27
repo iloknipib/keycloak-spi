9309e3a6d46bcd01c50015b0c76b3d0a60543450
sushilps123 <sushilps123@gmail.com>
2021-10-28 02:46:38 +0530

    AUTH-139: Write Rest Api to get user by mobile number
    This commit contains following things
    
    1. New Rest Api to get user by mobile number
    2. New Rest Api to create user for auto username creation + making email or mobile_number as mandatory
    3. Updating the jar with latest code build

--- dehaat-keycloak-spi/src/main/java/com/dehaat/rest/CustomRestEndPoints.java
+++ dehaat-keycloak-spi/src/main/java/com/dehaat/rest/CustomRestEndPoints.java
@@ -7,17 +7,30 @@ import com.dehaat.service.OTPGenerator;
 import com.dehaat.service.SMSSender;
 import com.dehaat.service.SendOTPService;
 
+import org.jboss.resteasy.annotations.cache.NoCache;
 import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.models.*;
+import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.models.utils.ModelToRepresentation;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.services.ErrorResponse;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager;
+import org.keycloak.services.managers.RealmManager;
+import org.keycloak.services.resources.admin.AdminAuth;
+import org.keycloak.services.resources.admin.AdminEventBuilder;
+import org.keycloak.services.resources.admin.UsersResource;
+import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
+import org.keycloak.services.resources.admin.permissions.AdminPermissions;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,6 +40,9 @@ import java.util.stream.Stream;
 public class CustomRestEndPoints {
     private final KeycloakSession session;
     private final AuthenticationManager.AuthResult auth;
+    private static final String MOBILE = "mobile_number";
+    private static final String ERR_INVALID_MOBILE = "Invalid Mobile Number";
+    private static final String ERR_EMPTY_EMAIL_OR_MOBILE = "Email or Mobile Number missing/invalid";
 
     public CustomRestEndPoints(KeycloakSession session) {
         this.session = session;
@@ -66,7 +82,7 @@ public class CustomRestEndPoints {
             MultivaluedMap<String, String> inputData = new MultivaluedMapImpl<>();
             user = AuthenticationUtils.createUser(inputData, session);
             AuthenticationUtils.generateSecret(user.getId(), session);
-            user.setSingleAttribute("mobile_number", mobile_number);
+            user.setSingleAttribute(MOBILE, mobile_number);
             user.setEnabled(true);
         }
 
@@ -82,7 +98,7 @@ public class CustomRestEndPoints {
             OTPGenerator otpGenerator = new GenerateOTPService(session, user, "HmacSHA1", length, ttl, 1);
             String otp = otpGenerator.createOTP();
             System.out.println(otp);
-            SMSSender sender = new SendOTPService(senderServiceURL, user.getFirstAttribute("mobile_number"), otp, ttl, token);
+            SMSSender sender = new SendOTPService(senderServiceURL, user.getFirstAttribute(MOBILE), otp, ttl, token);
             boolean isMailSent = sender.send();
             if (isMailSent) {
                 // 200 on success
@@ -93,56 +109,105 @@ public class CustomRestEndPoints {
         return Response.serverError().type(MediaType.APPLICATION_JSON).build();
     }
 
+    @GET
+    @NoCache
+    @Path("userbymobile")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<UserRepresentation> getUserByMobileNumber(@QueryParam(MOBILE) String mobileNumber) {
+        if(auth==null){
+            throw new NotAuthorizedException("Admin token is not provided");
+        }
+        AccessToken token = auth.getToken();
+        AdminAuth auth = authenticateRealmAdminRequest(token);
+        RealmModel realm = auth.getRealm();
 
-    /** In development
-     @POST
-     @Path("users")
-     @NoCache
-     @Consumes(MediaType.APPLICATION_JSON) public Response createUser(UserRepresentation userRepresentation) {
-     AccessToken token = auth.getToken();
-     AdminAuth auth = authenticateRealmAdminRequest(token);
-     RealmModel realm = auth.getRealm();
-     AdminPermissionEvaluator realmAuth = AdminPermissions.evaluator(session, realm, auth);
+        boolean isValidMobile = MobileNumberValidator.isValid(mobileNumber);
+        if (!isValidMobile) {
+            throw new ClientErrorException(ERR_INVALID_MOBILE, Response.Status.BAD_REQUEST);
+        }
 
-     AdminEventBuilder adminEvent = new AdminEventBuilder(realm, auth, session, session.getContext().getConnection());
-
-     String username;
-     username = userRepresentation.getUsername();
-     if (username == null || username.isEmpty()) {
-     username = KeycloakModelUtils.generateId();
-     userRepresentation.setUsername(username);
-     }
-
-     UsersResource usersResource = new UsersResource(realm, realmAuth, adminEvent);
-     return usersResource.createUser(userRepresentation);
-     }
-
-
-     protected AdminAuth authenticateRealmAdminRequest(AccessToken token) {
-     String realmName = token.getIssuer().substring(token.getIssuer().lastIndexOf('/') + 1);
-     RealmManager realmManager = new RealmManager(session);
-     RealmModel realm = realmManager.getRealmByName(realmName);
-     if (realm == null) {
-     throw new NotAuthorizedException("Unknown realm in token");
-     }
-     session.getContext().setRealm(realm);
+        List<UserRepresentation> userStream = session
+                .users()
+                .searchForUserByUserAttributeStream(realm, MOBILE, mobileNumber)
+                .map(userModel -> ModelToRepresentation.toRepresentation(session, session.getContext().getRealm(), userModel))
+                .collect(Collectors.toList());
+        return userStream;
+    }
 
 
-     AuthenticationManager.AuthResult authResult = new AppAuthManager.BearerTokenAuthenticator(session)
-     .setRealm(realm)
-     .authenticate();
+    @POST
+    @Path("users")
+    @NoCache
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(UserRepresentation userRepresentation) {
+        if(auth==null){
+            throw new NotAuthorizedException("Admin token is not provided");
+        }
+        AccessToken token = auth.getToken();
+        AdminAuth auth = authenticateRealmAdminRequest(token);
+        RealmModel realm = auth.getRealm();
 
-     if (authResult == null) {
-     throw new NotAuthorizedException("Bearer");
-     }
+        boolean isEmptyEmail = userRepresentation.getEmail()==null ? true:false;
+        boolean isValidMobile = false;
 
-     ClientModel client = realm.getClientByClientId(token.getIssuedFor());
-     if (client == null) {
-     throw new NotFoundException("Could not find client for authorization");
+        Map<String, List<String>> userAttributes = userRepresentation.getAttributes();
+        List<String> mobileNumberList=null;
+        if(userAttributes!=null) {
+            mobileNumberList = userAttributes.get(MOBILE);
+        }
 
-     }
+        if (mobileNumberList!=null && mobileNumberList.size() > 0) {
+            String mobileNumber = mobileNumberList.get(0);
+            isValidMobile = MobileNumberValidator.isValid(mobileNumber);
+            if (!isValidMobile) {
+                return ErrorResponse.error(ERR_INVALID_MOBILE, Response.Status.BAD_REQUEST);
+            }
+        }
 
-     return new AdminAuth(realm, authResult.getToken(), authResult.getUser(), client);
-     } **/
+        /** check occurance of atleast one mandatory parameter [email or mobile_number] **/
+        if (isEmptyEmail && !isValidMobile) {
+            return ErrorResponse.error("email or mobile number missing/invalid", Response.Status.BAD_REQUEST);
+        }
+
+        AdminPermissionEvaluator realmAuth = AdminPermissions.evaluator(session, realm, auth);
+        AdminEventBuilder adminEvent = new AdminEventBuilder(realm, auth, session, session.getContext().getConnection());
+        String username;
+        username = userRepresentation.getUsername();
+        if (username == null || username.isEmpty()) {
+            username = KeycloakModelUtils.generateId();
+            userRepresentation.setUsername(username);
+        }
+        UsersResource usersResource = new UsersResource(realm, realmAuth, adminEvent);
+        ResteasyProviderFactory.getInstance().injectProperties(usersResource);
+        return usersResource.createUser(userRepresentation);
+    }
+
+
+    protected AdminAuth authenticateRealmAdminRequest(AccessToken token) {
+        String realmName = token.getIssuer().substring(token.getIssuer().lastIndexOf('/') + 1);
+        RealmManager realmManager = new RealmManager(session);
+        RealmModel realm = realmManager.getRealmByName(realmName);
+        if (realm == null) {
+            throw new NotAuthorizedException("Unknown realm in token");
+        }
+        session.getContext().setRealm(realm);
+
+
+        AuthenticationManager.AuthResult authResult = new AppAuthManager.BearerTokenAuthenticator(session)
+                .setRealm(realm)
+                .authenticate();
+
+        if (authResult == null) {
+            throw new NotAuthorizedException("Bearer");
+        }
+
+        ClientModel client = realm.getClientByClientId(token.getIssuedFor());
+        if (client == null) {
+            throw new NotFoundException("Could not find client for authorization");
+
+        }
+
+        return new AdminAuth(realm, authResult.getToken(), authResult.getUser(), client);
+    }
 }
 
